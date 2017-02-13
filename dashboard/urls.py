a2b716a574896fe95e1267b19e69e27762261929
manikanta@revalsys.com <manikanta@revalsys.com>
2017-02-13 20:11:53 +0530

    New manipy2

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -11,8 +11,8 @@ import goedits.views
 
 urlpatterns = [
 	 url(r'^$', goedits.views.index, name='index'),
-	 url(r'^compare/$', goedits.views.compare, name='compare'),
-	 url(r'^pricing/$',goedits.views.pricing, name='pricing'),
-	 url(r'^contact$',goedits.views.contact, name='contact'),
-     url(r'^admin', include(admin.site.urls))
+	 # url(r'^compare/$', goedits.views.compare, name='compare'),
+	 # url(r'^pricing/$',goedits.views.pricing, name='pricing'),
+	 # url(r'^contact$',goedits.views.contact, name='contact'),
+     url(r'^admin', include(admin.site.urls)),
 ]
--- goedits/views.py
+++ goedits/views.py
@@ -10,24 +10,24 @@ def index(request):
     return HttpResponse(template.render(context, request))
 	
 	
-def compare(request):
-    template = loader.get_template('compare.html')
-    context = {
-        'data': 'compare',
-    }
-    return HttpResponse(template.render(context, request))
+# def compare(request):
+    # template = loader.get_template('compare.html')
+    # context = {
+        # 'data': 'compare',
+    # }
+    # return HttpResponse(template.render(context, request))
 	
-def pricing(request):
-    template = loader.get_template('pricing.html')
-    context = {
-        'data': 'compare',
-    }
-    return HttpResponse(template.render(context, request))
+# def pricing(request):
+    # template = loader.get_template('pricing.html')
+    # context = {
+        # 'data': 'compare',
+    # }
+    # return HttpResponse(template.render(context, request))
 	
-def contact(request):
-    template = loader.get_template('contact.html')
-    context = {
-        'data': 'compare',
-    }
-    return HttpResponse(template.render(context, request))
+# def contact(request):
+    # template = loader.get_template('contact.html')
+    # context = {
+        # 'data': 'compare',
+    # }
+    # return HttpResponse(template.render(context, request))
 	
